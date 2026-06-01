c910c528eb15360e3a4dc434b2c994e21e0ff1bd
KONDA-HARIKA <harikakonda2007@gmail.com>
2026-06-01 17:16:49 +0530

    Add files via upload

---
+++ a.js
@@ -0,0 +1,78 @@
+const images = document.querySelectorAll(".gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.querySelector(".lightbox-img");
+const closeBtn = document.querySelector(".close");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+let currentIndex = 0;
+
+/* Open Lightbox */
+
+images.forEach((img, index) => {
+
+  img.addEventListener("click", () => {
+
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+    currentIndex = index;
+
+  });
+
+});
+
+/* Close Lightbox */
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+/* Next Image */
+
+nextBtn.addEventListener("click", () => {
+
+  currentIndex++;
+
+  if(currentIndex >= images.length){
+    currentIndex = 0;
+  }
+
+  lightboxImg.src = images[currentIndex].src;
+
+});
+
+/* Previous Image */
+
+prevBtn.addEventListener("click", () => {
+
+  currentIndex--;
+
+  if(currentIndex < 0){
+    currentIndex = images.length - 1;
+  }
+
+  lightboxImg.src = images[currentIndex].src;
+
+});
+
+/* Filter Images */
+
+function filterImages(category){
+
+  const allImages = document.querySelectorAll(".image");
+
+  allImages.forEach((item) => {
+
+    if(category === "all"){
+      item.style.display = "block";
+    }
+    else if(item.classList.contains(category)){
+      item.style.display = "block";
+    }
+    else{
+      item.style.display = "none";
+    }
+
+  });
+
+}
